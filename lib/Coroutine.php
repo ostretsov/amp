dd303dbc9009f76234b164eba814cffb504a9f44
Niklas Keller <me@kelunik.com>
2017-12-07 19:23:25 +0100

    Recreate watcher if it has been triggered

--- lib/Coroutine.php
+++ lib/Coroutine.php
@@ -42,6 +42,10 @@ final class Coroutine implements Promise {
     public function __construct(\Generator $generator) {
         $this->generator = $generator;
         $this->timeoutWatcher = Loop::delay(1000, function () {
+            $this->timeoutWatcher = Loop::delay(1000, function () {
+                fwrite(STDERR, $this->trace . "\r\n");
+            });
+
             fwrite(STDERR, $this->trace . "\r\n");
         });
         $this->trace = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
